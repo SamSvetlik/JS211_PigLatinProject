f865db8fb235edc93d04952a9843ea7d44c99958
SamSvetlik <sam.svetlik@gmail.com>
2022-06-26 16:21:09 -0500

    works with single words, but not sentences.

--- main.js
+++ main.js
@@ -13,8 +13,44 @@ const rl = readline.createInterface({
 
 const pigLatin = (word) => {
 
-  // Your code here
-
+  let splitword = word.toLowerCase().trim().split("")
+  if (
+    splitword[0] === "a" || 
+    splitword[0] === "e" || 
+    splitword[0] === "i" ||
+    splitword[0] === "o" ||
+    splitword[0] === "u"  )
+  { return word + "yay"}
+  else if (
+    splitword[1] === "a" || 
+    splitword[1] === "e" || 
+    splitword[1] === "i" ||
+    splitword[1] === "o" ||
+    splitword[1] === "u")
+    { let yay1 = splitword.shift();
+      return splitword.join("") + yay1 + "ay"
+    }
+    else if (
+      splitword[2] === "a" || 
+      splitword[2] === "e" || 
+      splitword[2] === "i" ||
+      splitword[2] === "o" ||
+      splitword[2] === "u")
+    {
+      let yay2 = splitword.splice(0, 2)
+      return splitword.join("") + yay2.join("") + "ay"
+    }
+    else if (
+      splitword[3] === "a" || 
+      splitword[3] === "e" || 
+      splitword[3] === "i" ||
+      splitword[3] === "o" ||
+      splitword[3] === "u")
+    {
+      let yay3 = splitword.splice(0, 3)
+      return splitword.join("") + yay3.join("") + "ay"
+    }
+    else return "That is not an English word."
 }
 
 // the first function called in the program to get an input from the user
